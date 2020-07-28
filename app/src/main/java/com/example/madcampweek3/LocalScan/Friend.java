c737357680113f7507b581fa67353a7e3ca9b9fb
KyusungSeo <seo3650@kaist.ac.kr>
2020-07-28 17:40:44 +0900

    Add localscan

--- app/src/main/java/com/example/madcampweek3/LocalScan/Friend.java
+++ app/src/main/java/com/example/madcampweek3/LocalScan/Friend.java
@@ -2,8 +2,10 @@ package com.example.madcampweek3.LocalScan;
 
 public class Friend {
     String name;
+    Number score;
 
-    public Friend(String name) {
+    public Friend(String name, Number score) {
         this.name = name;
+        this.score = score;
     }
 }
