943f6c9258c1f05416ebd6fed1a9d337eb974edb
KyusungSeo <seo3650@kaist.ac.kr>
2020-07-28 21:06:05 +0900

    Add image

--- app/src/main/java/com/example/madcampweek3/LocalScan/Friend.java
+++ app/src/main/java/com/example/madcampweek3/LocalScan/Friend.java
@@ -3,21 +3,21 @@ package com.example.madcampweek3.LocalScan;
 import android.graphics.Bitmap;
 
 public class Friend {
+    String id;
     String name;
     Number score;
     String phoneNumber;
     Bitmap profile;
 
-    public Friend(String name, Number score, String phoneNumber) {
+    public Friend(String id, String name, Number score, String phoneNumber) {
+        this.id = id;
         this.name = name;
         this.score = score;
         this.phoneNumber = phoneNumber;
-        this.profile = profile;
     }
 
-    public void setProfile(Bitmap profile) {
-        this.profile = profile;
-    }
+    public void setProfile(Bitmap profile) { this.profile = profile; }
+    public String getId() { return this.id; }
 
 
 }
